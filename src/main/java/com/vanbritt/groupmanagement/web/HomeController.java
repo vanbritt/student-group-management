bb53470eeef8b77571e6ca60979a04dfd832c328
vanbritt <bricevanec@gmail.com>
2022-10-19 05:53:18 +0100

    innitial commit

---
+++ src/main/java/com/vanbritt/groupmanagement/web/HomeController.java
@@ -0,0 +1,17 @@
+package com.vanbritt.groupmanagement.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HomeController {
+
+    @GetMapping(path = "/")
+    public ResponseEntity<String> home(){
+        return ResponseEntity.status(HttpStatus.OK).body("Hello World ");
+    }
+}
---
+++ src/test/java/com/vanbritt/groupmanagement/GroupManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanbritt.groupmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
